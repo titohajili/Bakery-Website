9d224ea5e9bb3643a27d3b86e8d5f6070d7523d7
titohajili <maqatoto963@gmail.com>
2024-02-01 17:46:11 +0400

    Buttons, Add Blur Header, Section New, Section About

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,3 +26,13 @@ const linkAction = () =>{
 }
 
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+//=======ADD BLUR HEADER
+
+const blurHeader = () => {
+    const header = document.getElementById('header')
+
+    this.scrollY >= 50 ? header.classList.add('blur-header')
+                       : header.classList.remove('blur-header')
+}
+window.addEventListener('scroll', blurHeader)
